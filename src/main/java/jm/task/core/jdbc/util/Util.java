20184b8e30efc42b5bb24eacaa59b73645620e6d
ziliboba-dev <82903253+ziliboba-dev@users.noreply.github.com>
2021-07-14 15:35:44 +0300

    Задача 1.1.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,11 +8,11 @@ public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/mydbtest";
-    private Connection connection;
+    private static Connection connection;
 
     public Util() {}
 
-    public Connection getConnection() {
+    public static Connection getConnection() {
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
