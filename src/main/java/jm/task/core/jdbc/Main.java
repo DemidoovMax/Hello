91ba231a943f3f62477fcaa45442982896393e8e
ziliboba-dev <82903253+ziliboba-dev@users.noreply.github.com>
2021-07-15 19:04:15 +0300

    Задача 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,12 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Ivan", "Petrov", (byte) 20);
         System.out.println("User с именем Ivan добавлен в базу данных");
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -19,80 +19,61 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try {
             session = factory.getCurrentSession();
             session.beginTransaction();
             session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (id int not null AUTO_INCREMENT, name VARCHAR(40)," +
                     "lastName VARCHAR(40), age INT, primary key (id))").executeUpdate();
             session.getTransaction().commit();
-        } finally {
-            session.close();
-        }
     }
 
     @Override
     public void dropUsersTable() {
-        try {
             session = factory.getCurrentSession();
             session.beginTransaction();
             session.createSQLQuery("drop table if EXISTS users").executeUpdate();
             session.getTransaction().commit();
-        } finally {
             session.close();
-        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try {
             User user = new User(name, lastName, age);
             session = factory.getCurrentSession();
             session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
-        } finally {
             session.close();
-        }
     }
 
     @Override
     public void removeUserById(long id) {
-        try {
             session = factory.getCurrentSession();
             session.beginTransaction();
             User user = session.get(User.class, id);
             session.delete(user);
             session.getTransaction().commit();
-        } finally {
             session.close();
-        }
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        try {
             session = factory.getCurrentSession();
             session.beginTransaction();
             List<User> userList = session.createQuery("from User", User.class).getResultList();
             session.getTransaction().commit();
-            return userList;
-        } finally {
-
             session.close();
-        }
+            return userList;
+
 
     }
 
     @Override
     public void cleanUsersTable() {
-        try {
             session = factory.getCurrentSession();
             session.beginTransaction();
             session.createQuery("delete User").executeUpdate();
             session.getTransaction().commit();
-        } finally {
             session.close();
-        }
     }
 }
