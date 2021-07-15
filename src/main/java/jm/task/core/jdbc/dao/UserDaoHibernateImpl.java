e2d6639af35560d7f165401d7e182071947dbd40
ziliboba-dev <82903253+ziliboba-dev@users.noreply.github.com>
2021-07-15 14:17:05 +0300

    Задача 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,41 +1,98 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private Session session;
+    private final SessionFactory factory;
 
-    public UserDaoHibernateImpl() {}
+    public UserDaoHibernateImpl() {
+        factory = Util.getFactory();
+    }
 
 
     @Override
     public void createUsersTable() {
-
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (id int not null AUTO_INCREMENT, name VARCHAR(40)," +
+                    "lastName VARCHAR(40), age INT, primary key (id))").executeUpdate();
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            session.createSQLQuery("drop table if EXISTS users").executeUpdate();
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            User user = new User(name, lastName, age);
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
 
     }
 
     @Override
     public List<User> getAllUsers() {
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            List<User> userList = session.createQuery("from User", User.class).getResultList();
+            session.getTransaction().commit();
+            return userList;
+        } finally {
 
-        return null;
+            session.close();
+        }
+
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc.util;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class Util {
@@ -9,6 +17,7 @@ public class Util {
     private static final String PASSWORD = "root";
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/mydbtest";
     private static Connection connection;
+    private static SessionFactory factory;
 
     public Util() {}
 
@@ -20,6 +29,29 @@ public class Util {
         }
         return connection;
     }
+
+    public static SessionFactory getFactory() {
+        if (factory == null) {
+            Configuration cfg = new Configuration();
+            Properties prop = new Properties();
+            prop.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            prop.put(Environment.URL, "jdbc:mysql://localhost:3306/mydbtest");
+            prop.put(Environment.USER, "root");
+            prop.put(Environment.PASS, "root");
+            prop.put(Environment.SHOW_SQL, "true");
+            prop.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            prop.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            prop.put(Environment.HBM2DDL_AUTO, "create");
+
+            cfg.setProperties(prop);
+            cfg.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(cfg.getProperties()).build();
+            factory = cfg.buildSessionFactory(serviceRegistry);
+        }
+        return factory;
+    }
+
 
 
 
